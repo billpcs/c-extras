483f60d63ee589d713914d9eb1d8cf112a3695ba
Bill Panagiotopoulos <panagiotopoulosbill@yahoo.com>
2014-11-01 00:32:49 +0200

    Update easylist.c

--- src/easylist.c
+++ src/easylist.c
@@ -157,7 +157,7 @@ int main(void)
  	for (i = 0 ;  i < 20 ; i++){
  		append(i*i , &test) ; 
  	}
- 	printlist(test) ;
+ 	printlist(test) ; // [0,1,4,9,16,25,36,49,64,81,100,121,144,169,196,225,256,289,324,361]
 	
 	return 0;
 }
