02f33436b6a57f02f5649e15cb5b3f11f946d049
Bill Panagiotopoulos <panagiotopoulosbill@yahoo.com>
2014-11-01 14:28:32 +0200

    Update easylist.c

--- src/easylist.c
+++ src/easylist.c
@@ -7,6 +7,9 @@ typedef struct node {
 } LIST ;
 
 
+void printlist( LIST *Start ) ; 
+
+
 
 /*
 Generates a new empty list.
@@ -36,13 +39,12 @@ int filllist( int len , LIST **Head )
 	(*Head)->next = NULL ;
 
 	p = *Head ; 
-
 	for( i = 0 ; i < len-1 ; i++ ){
 		p->next = malloc( sizeof(LIST) )  ;
 		if ( p->next == NULL )
 			return -1 ;
 		p = p->next ;
-		p->num = 0 ;	
+		p->num = 0 ;
 	}
 	p->next = NULL ;
 	return len;
@@ -82,23 +84,6 @@ int append( int elem ,  LIST **Head )
 
 
 /*
-Prints out the contents of the list.
-*/
-void printlist( LIST *Start )
-{
-	printf("[");
-	while ( Start->next != NULL )
-	{
-		printf( "%d,", Start->num ) ;
-		Start = (Start)->next ;
-	}
-	printf( "%d", (Start)->num ) ; 
-	printf("] \n");
-}
-
-
-
-/*
 Inserts an element 'elem' in the place index of
 the list. Returns 1 if it inserted it ,
 0 if it did not insert it and -1 if there was 
@@ -112,15 +97,15 @@ int insert( int index , int elem ,  LIST **Head )
 
 	// If the given index is not valid 
 	if (index < 0) return -1 ; 
-
-	//	If the list is empty and index > 0 
+	// If the list is empty and index > 0 
 	if ( *Head == NULL && index > 0 ) return -1 ;
 	else if (*Head == NULL && index == 0){
 		*Head = malloc( sizeof(LIST) ) ;
 		if ( *Head == NULL )
 			return -1 ;
 		(*Head)->num = elem ; 
-		(*Head)->next = NULL ; 	
+		(*Head)->next = NULL ;
+		return 1 ;  	
 	}
 		
 
@@ -128,7 +113,6 @@ int insert( int index , int elem ,  LIST **Head )
 	p = *Head ;
 	int place = 0 ; 
 	while ( p != NULL ){
-		printf("%d\n", place );
 		if ( place == index ){
 			p1 = p ;
 			p2 = p->next ; 
@@ -147,17 +131,31 @@ int insert( int index , int elem ,  LIST **Head )
 	return 0 ;
 }
 
-
-int main(void)
+/*
+Prints out the contents of the list.
+*/
+void printlist( LIST *Start )
 {
+	printf("[ ");
+	while ( Start != NULL ){
+		printf( "%d ", Start->num ) ;
+		Start = (Start)->next ;
+	}
+	printf("] \n");
+}
 
+
+
+
+int main(void){
 	LIST *test ;
-	newlist(&test) ;  
- 	int i ;
- 	for (i = 0 ;  i < 20 ; i++){
- 		append(i*i , &test) ; 
- 	}
- 	printlist(test) ; // [0,1,4,9,16,25,36,49,64,81,100,121,144,169,196,225,256,289,324,361]
-	
+	newlist(&test) ; 
+ 	int i ; 
+ 	for (i=0 ; i<5 ; i++){
+ 		insert(0 , i , &test) ; 
+ 		printlist(test) ; 
+ 	} 
+ 	insert(5 , 100 , &test) ; 
+ 	printlist(test) ; 
 	return 0;
 }
